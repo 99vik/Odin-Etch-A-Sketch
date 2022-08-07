f07d20f206785e2aae37b059ae74cedf9206af85
Viktor Miric <viktorm47@gmail.com>
2022-08-07 11:39:10 +0200

    Added drawing only if mouse is clicked too feature

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//grid size changing
 const container = document.querySelector('.container');
 let x=16;
 const gridBtn = document.querySelector('button.grid-size');
@@ -9,40 +10,6 @@ gridBtn.addEventListener('click',() => {
     }
     setGrid();
 });
-function randomColorGen () {
-    function randomNum() {
-        return Math.floor(Math.random() * (266))
-    }
-    return `rgb(${randomNum()},${randomNum()},${randomNum()})`;
-}
-
-function changeColor(e) {
-    e.target.style.backgroundColor = randomColorGen();
-}
-
-function addDarknes(e) {
-    let oldBrigthness = e.target.classList[1].slice(-1);
-    if (oldBrigthness > 0) {
-        let newBrightness = oldBrigthness-1;
-        e.target.classList.replace(`bright-${oldBrigthness}`,`bright-${newBrightness}`);
-    } else return;
-}
-
-for (let i=1;i<=x;i++) {
-    const row = document.createElement('div');
-    row.classList = 'row';
-    container.appendChild(row);
-    for (let j=1;j<=x;j++) {
-        const box = document.createElement('div');
-        box.classList = 'box bright-9';
-        row.appendChild(box);
-        box.addEventListener('mouseover', (e) => {
-            changeColor(e);
-            addDarknes(e);
-        });
-    }
-}   
-
 function setGrid() {
     container.textContent = "";
     for (let i=1;i<=x;i++) {
@@ -53,11 +20,58 @@ function setGrid() {
             const box = document.createElement('div');
             box.classList = 'box bright-9';
             row.appendChild(box);
-            box.addEventListener('mouseover', (e) => {
+            box.addEventListener('onmousedown', (e) => {
                 changeColor(e);
                 addDarknes(e);
             });
         }
     }   
 }
+
+//color change
+function randomColorGen () {
+    function randomNum() {
+        return Math.floor(Math.random() * (266))
+    }
+    return `rgb(${randomNum()},${randomNum()},${randomNum()})`;
+}
+function changeColor(e) {
+    e.target.style.backgroundColor = randomColorGen();
+}
+function addDarknes(e) {
+    let oldBrigthness = e.target.classList[1].slice(-1);
+    if (oldBrigthness > 0) {
+        let newBrightness = oldBrigthness-1;
+        e.target.classList.replace(`bright-${oldBrigthness}`,`bright-${newBrightness}`);
+    } else return;
+}
+
+//initial creation of the grid
+for (let i=1;i<=x;i++) {
+    const row = document.createElement('div');
+    row.classList = 'row';
+    container.appendChild(row);
+    for (let j=1;j<=x;j++) {
+        const box = document.createElement('div');
+        box.classList = 'box bright-9';
+        row.appendChild(box);
+        box.addEventListener('mouseover', (e) => {
+            if (isMouseDown) {
+                changeColor(e);
+                addDarknes(e);
+            }
+        });
+    }
+}   
+
+//drawing only if mouse is down too feature
+let isMouseDown;
+const mouseDown = window.addEventListener('mousedown', (e) => {
+    isMouseDown = 1;
+});
+const mouseUp = window.addEventListener('mouseup', (e) => {
+    isMouseDown = 0;
+});
+
+
 
